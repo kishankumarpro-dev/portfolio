6ca092324572d131638a91e43960cbc2ad5c1c91
kishankumarpro-dev <kishankumarpro1@gmail.com>
2026-07-24 19:02:07 +0530

    Update Home2.js

--- src/Components/Home/Home2.js
+++ src/Components/Home/Home2.js
@@ -63,7 +63,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/kishan-k9"
+                  href="https://github.com/kishankumar-dev"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
